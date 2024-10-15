e092031c2568a2498e613363444745bd08038384
9ZonD6 <bsemer@mail.ru>
2024-10-15 22:15:34 +0500

    Add 5 lesson

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from messenger.views import index
+
+from messenger import views
 
 
 urlpatterns = [
-    path('', index),
-]
+    path('posts/', views.PostListView.as_view(), name='Posts'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='PostDetail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='PostUpdate'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='PostDelete'),
+    path('post/create/', views.PostCreateView.as_view(), name='PostCreate'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- messenger/views.py
+++ messenger/views.py
@@ -1,6 +1,45 @@
-from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from .models import Post
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world.")
+class PostListView(ListView):
+    template_name = 'mess_temp/mess_list.html'
+    model = Post
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    template_name = 'mess_temp/mess_detail.html'
+    model = Post
+    context_object_name = 'post'
+
+
+class PostUpdateView(UpdateView):
+    template_name = 'mess_temp/mess_form.html'
+    model = Post
+    context_object_name = 'post'
+    fields = ['post_subject', 'author', 'post_text']
+
+    def get_success_url(self):
+        return reverse_lazy('PostDetail', kwargs={'pk': self.object.pk})
+
+
+class PostDeleteView(DeleteView):
+    template_name = 'mess_temp/mess_confirm_delete.html'
+    model = Post
+    context_object_name = 'post'
+
+    def get_success_url(self):
+        return reverse_lazy('Posts')
+
+
+class PostCreateView(CreateView):
+    template_name = 'mess_temp/mess_create.html'
+    model = Post
+    context_object_name = 'post'
+    fields = ['post_subject', 'author', 'post_text']
+
+    def get_success_url(self):
+        return reverse_lazy('Posts')
